3696fc828f7748d9e09b931f65a3e05487c4ac97
RidhanParvendhan <ridhanparvendhan@gmail.com>
2023-05-22 22:52:40 +0300

    final-commit

--- employee-management-system-backend/employee-system-api/src/main/java/com/example/employee/EmployeeSystemApiApplication.java
+++ employee-management-system-backend/employee-system-api/src/main/java/com/example/employee/EmployeeSystemApiApplication.java
@@ -1,4 +1,4 @@
-package com.fullstackproj.employee;
+package com.example.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeSystemApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EmployeeSystemApiApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSystemApiApplication.class, args);
+	}
 
 }
